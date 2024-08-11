3f6350dac6e2e8710e4c6fe64905ddf54f07d1f7
Stanley-Dam <42673142+Stanley-Dam@users.noreply.github.com>
2024-08-11 13:01:10 +0200

    Update server properties and enhance WorldsDirectoryManager
    Added new properties to server configurations, including enabling RCON. Enhanced `WorldsDirectoryManager` logic to handle Nether world loading conditionally and refactored directory copy methods for improved clarity and functionality.

--- src/main/java/gg/bonka/mirage/filesystem/WorldsDirectoryManager.java
+++ src/main/java/gg/bonka/mirage/filesystem/WorldsDirectoryManager.java
@@ -1,6 +1,8 @@
 package gg.bonka.mirage.filesystem;
 
+import gg.bonka.mirage.misc.ConsoleLogger;
 import org.bukkit.Bukkit;
+import org.codehaus.plexus.util.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,14 +30,23 @@ public class WorldsDirectoryManager {
         serverProperties.load(Files.newInputStream(new File(Bukkit.getWorldContainer(), "server.properties").toPath()));
 
         String worldName = serverProperties.getProperty("level-name");
+        boolean netherEnabled = Boolean.getBoolean(serverProperties.getProperty("allow-nether"));
 
-        //Save the existing worlds when this plugin is enabled for the first time!
-        if(worldsDirectory.mkdirs()) {
-            saveWorlds(worldName, String.format("%s_the_end", worldName));
+        String[] worldNames = new String[netherEnabled ? 3 : 2];
+        worldNames[0] = worldName;
+        worldNames[1] = String.format("%s_the_end", worldName);
 
-            if(Boolean.getBoolean(serverProperties.getProperty("allow-nether")))
-                saveWorld(String.format("%s_nether", worldName));
+        if(netherEnabled) {
+            worldNames[2] = String.format("%s_nether", worldName);
         }
+
+        if(worldsDirectory.mkdirs()) {
+            //Save the existing worlds when this plugin is enabled for the first time!
+            saveWorlds(worldNames);
+            return;
+        }
+
+        loadWorlds(worldNames);
     }
 
     /**
@@ -63,7 +74,38 @@ public class WorldsDirectoryManager {
         //noinspection ResultOfMethodCallIgnored
         saveDirectory.mkdirs();
 
-        copyDirectory(worldDirectory, saveDirectory, worldName, path -> !path.getFileName().toString().endsWith(".lock"));
+        copyDirectory(worldDirectory, saveDirectory.toPath(), worldName, path -> !path.getFileName().toString().endsWith(".lock"));
+    }
+
+    /**
+     * Loads the specified worlds into the game.
+     *
+     * @param worldNames The names of the worlds to load.
+     * @throws IOException If an error occurs while loading the worlds.
+     */
+    public void loadWorlds(String... worldNames) throws IOException {
+        for(String worldName : worldNames) {
+            loadWorld(worldName);
+        }
+    }
+
+    /**
+     * Loads the specified world into the game.
+     *
+     * @param worldName The name of the world to load.
+     * @throws IOException If an error occurs while loading the world.
+     */
+    public void loadWorld(String worldName) throws IOException {
+        File worldDirectory = new File(Bukkit.getWorldContainer(), worldName);
+        File saveDirectory = new File(worldsDirectory, worldName);
+
+        if(!saveDirectory.exists()) {
+            ConsoleLogger.error(String.format("The world %s could not be found!", worldName));
+            return;
+        }
+
+        FileUtils.deleteDirectory(worldDirectory);
+        copyDirectory(saveDirectory.toPath(), worldDirectory.toPath(), worldName, path -> true);
     }
 
     /**
@@ -75,11 +117,11 @@ public class WorldsDirectoryManager {
      * @param predicate     a predicate used to filter the files to be copied
      * @throws IOException if an I/O error occurs during the copy process
      */
-    public void copyDirectory(Path source, File saveDirectory, String worldName, Predicate<Path> predicate) throws IOException {
+    private void copyDirectory(Path source, Path saveDirectory, String worldName, Predicate<Path> predicate) throws IOException {
         try(Stream<Path> stream = Files.walk(source)) {
             stream.filter(predicate).forEach(path -> {
                 try {
-                    Path targetPath = saveDirectory.toPath().resolve(source.relativize(path));
+                    Path targetPath = saveDirectory.resolve(source.relativize(path));
                     Files.copy(path, targetPath, StandardCopyOption.REPLACE_EXISTING);
                 } catch (IOException e) {
                     throw new RuntimeException(String.format("Error occurred while saving world: %s to the worlds directory.", worldName), e);
